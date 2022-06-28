d92d2343093a71ebc1973babd8c22ad64426058a
İsa DUVAN <isaduvan@outlook.com>
2022-06-28 23:44:07 +0300

    e

--- packages/sample/hello/hello.js
+++ packages/sample/hello/hello.js
@@ -1,12 +1,12 @@
-import fetch from "node-fetch";
-import crypto from "crypto";
-import OAuth   from "oAuth-1.0a";
+import { fetch } from "node-fetch";
+import { crypto } from "crypto";
+import { OAuth } from "oAuth-1.0a";
 
 function main(args) {
-    let name = args.name || 'yabancı'
-    let greeting = 'Hello ' + name + '!'
-    console.log(greeting)
-    return {"body": greeting}
-  }
+  let name = args.name || "yabancı";
+  let greeting = "Hello " + name + "!";
+  console.log(greeting);
+  return { body: greeting };
+}
 
-exports.main = main
+exports.main = main;
