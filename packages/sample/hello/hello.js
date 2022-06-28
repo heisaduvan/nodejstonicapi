e08f0ff0009f0688d9e8cfc21f48c268e099ab23
İsa DUVAN <isaduvan@outlook.com>
2022-06-28 23:17:57 +0300

    first commit

--- packages/sample/hello/hello.js
+++ packages/sample/hello/hello.js
@@ -1,3 +1,7 @@
+import fetch from "node-fetch";
+import crypto from "crypto";
+import OAuth   from "oAuth-1.0a";
+
 function main(args) {
     let name = args.name || 'stranger'
     let greeting = 'Hello ' + name + '!'
