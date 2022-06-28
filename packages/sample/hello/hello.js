d6f3de47a79b1ccdda156c1c646f1610f52c84cf
İsa DUVAN <isaduvan@outlook.com>
2022-06-28 23:24:54 +0300

    function edited

--- packages/sample/hello/hello.js
+++ packages/sample/hello/hello.js
@@ -3,7 +3,7 @@ import crypto from "crypto";
 import OAuth   from "oAuth-1.0a";
 
 function main(args) {
-    let name = args.name || 'stranger'
+    let name = args.name || 'yabancı'
     let greeting = 'Hello ' + name + '!'
     console.log(greeting)
     return {"body": greeting}
